22579d4ac3eeee4a3c8ab8860cbe6af1851299d0
MariusROBERT <39240644+MariusROBERT@users.noreply.github.com>
2020-03-12 11:34:47 +0100

    Create Jeu de la vie 2.py

---
+++ Jeu de la vie 2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+
+
+
+# Importation des modules
+from tkinter import *
+from tkinter.messagebox import *
+from random import *
+import time
+
+
+#Variables
+taille = 20
+pourcentageVivant = 17
+etatCell = []   #map avec état cell à l'étape a
+etatCell2 = []  #map avec état cell à l'étape a+1
+#speed = IntVar()
+pause = 3
+random = False
+
