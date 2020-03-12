7d14cda754dde5d54d2300398aff1fa6174077c8
MariusROBERT <39240644+MariusROBERT@users.noreply.github.com>
2020-03-12 21:17:49 +0100

    Update Jeu de la vie 2.py

--- Jeu de la vie 2.py
+++ Jeu de la vie 2.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 # -- coding: utf-8 --
 
+
+
 # Importation des modules
 from tkinter import *
 from tkinter.messagebox import *
 from random import *
 import time
 
+
+
 #Variables
 taille = 20
 pourcentageVivant = 17
 etatCell = []  #map avec état cell à l'étape n
 etatCell2 = []  #map avec état cell à l'étape n+1
-pause = 3
+pause = 1
 random = False
 
 #Fenetre Principale
@@ -23,6 +27,9 @@ Fenetre.title("Jeu de la Vie by Marius")
 
 
 def generation():
+    global pourcentageVivant
+
+    print("Generation")
 
     for x in range(taille):
         etatCell.append([])
@@ -32,7 +39,7 @@ def generation():
             etatCell[x].append([])
             etatCell2[x].append([])
 
-            if randint(0, 100) < poucentageVivant:
+            if randint(0, 100) < pourcentageVivant:
                 etatCell[x][y] = True
             else:
                 etatCell[x][y] = False
@@ -43,54 +50,60 @@ def generation():
 
 def initialisationFenetre():
 
+    print("initialisationFenetre")
+
     for x in range(taille):
         for y in range(taille):
             if etatCell[x][y] == True:
                 Label(
                     Fenetre,
-                    text="   ",
-                    relief=GROOVE,
-                    borderwidth=1,
-                    bg="chartreuse").grid(
-                        row=y, column=x, sticky="news")
+                    text = "   ",
+                    relief = GROOVE,
+                    borderwidth = 1,
+                    bg = "chartreuse").grid(
+                        row = y, column = x, sticky = "news")
             else:
                 Label(
                     Fenetre,
-                    text="   ",
-                    relief=GROOVE,
-                    borderwidth=1,
-                    bg="light grey").grid(
-                        row=y, column=x, sticky="news")
+                    text = "   ",
+                    relief = GROOVE,
+                    borderwidth = 1,
+                    bg = "light grey").grid(
+                        row = y, column = x, sticky = "news")
 
 
 def compteur(x, y):
+
+    print("compteur")
 
     cellVivantesAutour = 0
 
     for xadd in range(-1, 2):
         for yadd in range(-1, 2):
 
-            if x + xadd >= taille and y + yadd >= taille:
+            if x + xadd >=  taille and y + yadd >=  taille:
                 if etatCell[x + xadd - taille][y + yadd - taille] == True:
-                    cellVivantesAutour += 1
+                    cellVivantesAutour +=  1
 
-            elif x + xadd >= taille:
+            elif x + xadd >=  taille:
                 if etatCell[x + xadd - taille][y + yadd] == True:
-                    cellVivantesAutour += 1
+                    cellVivantesAutour +=  1
 
-            elif y + yadd >= taille:
+            elif y + yadd >=  taille:
                 if etatCell[x + xadd][y + yadd - taille] == True:
-                    cellVivantesAutour += 1
+                    cellVivantesAutour +=  1
 
-            elif i != 0 or j != 0:
+            elif x !=  0 or y !=  0:
                 if etatCell[x + xadd][y + yadd] == True:
-                    cellVivantesAutour += 1
+                    cellVivantesAutour +=  1
 
     return cellVivantesAutour
 
 
 def etapeSuivante():
     global etatCell, etatCell2
+
+    print("etapeSuivante")
 
     for x in range(taille):
         for y in range(taille):
@@ -110,18 +123,23 @@ def etapeSuivante():
 
 def actualisationFenetre():
 
+    print("actualisationFenetre")
+
     for x in range(taille):
         for y in range(taille):
             if etatCell[x][y] == True:
                 Fenetre.grid_slaves(
-                    row=y, column=x)[0].configure(bg="chartreuse")
+                    row = y, column = x)[0].configure(bg = "chartreuse")
 
             else:
                 Fenetre.grid_slaves(
-                    row=y, column=x)[0].configure(bg="light grey")
+                    row = y, column = x)[0].configure(bg = "light grey")
 
 
 def changement():
+
+    print("changement")
+
     etapeSuivante()
     actualisationFenetre()
 
@@ -132,12 +150,17 @@ test = 0
 def bouclePrincipale():
     global test
 
+    print("bouclePrincipale")
+
     if test == 1:
         changement()
+        print("changement bouclePrincipale")
 
     test = 1
 
-    Fenetre.adter(pause * 1000, boucle)
+    print("bouclePrincipale 2")
+
+    Fenetre.after(pause * 1000, bouclePrincipale)
 
 
 #Préparation
@@ -147,7 +170,7 @@ initialisationFenetre()
 
 #Lancement
 
-bouclePrincipale
+bouclePrincipale()
 
 Fenetre.mainloop
 
