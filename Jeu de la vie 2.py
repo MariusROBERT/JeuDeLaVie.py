94f185e7efa732293b320664e08d6e22a06f3abd
MariusROBERT <39240644+MariusROBERT@users.noreply.github.com>
2020-03-15 11:14:26 +0100

    Update Jeu de la vie 2.py

--- Jeu de la vie 2.py
+++ Jeu de la vie 2.py
@@ -8,41 +8,83 @@ from tkinter import *
 from tkinter.messagebox import *
 from random import *
 import time
+from tkinter.filedialog import *
+import tkinter.colorchooser
 
 
 
 #Variables
-taille = 20
-pourcentageVivant = 17
+taille = 5
+pourcentageVivant = 20
 etatCell = []  #map avec état cell à l'étape n
 etatCell2 = []  #map avec état cell à l'étape n+1
-pause = 1
+pause = 5
 random = False
+choixCouleur = False
+couleurVivant = "chartreuse"
+
+
 
 #Fenetre Principale
 Fenetre = Tk()
 Fenetre.title("Jeu de la Vie by Marius")
 
-#Génération
 
+
+#Options
+if choixCouleur == True:
+    couleurVivant = (tkinter.colorchooser.askcolor(color=None))[1]
+
+
+if random == False:
+    filepath = askopenfilename(title = "Ouvrir une map",filetypes = [("txt files", ".txt")])
+    fichier = open(filepath, "r")      #Importation du fichier map.txt
+    contenuFichier = fichier.read()
+    fichier.close()
+
+    taille = int(contenuFichier.split("\n")[0])     #Taille de la map sur la 1ère ligne
+    print(taille)
+
+
+
+
+
+#Génération
 
 def generation():
     global pourcentageVivant
 
-    print("Generation")
 
-    for x in range(taille):
-        etatCell.append([])
+    for y in range(taille):
+        etatCell.append([])     #Tableaux
         etatCell2.append([])
 
-        for y in range(taille):
-            etatCell[x].append([])
-            etatCell2[x].append([])
+        Fenetre.columnconfigure(y, weight = 1)  #Pour éviter la déformation
+        Fenetre.rowconfigure(y, weight = 1)     #en étirant la fenetre
 
-            if randint(0, 100) < pourcentageVivant:
-                etatCell[x][y] = True
-            else:
-                etatCell[x][y] = False
+        for x in range(taille):
+            etatCell[y].append([])  #Double tableaux
+            etatCell2[y].append([])
+
+            if random == True:
+
+                if randint(0, 100) < pourcentageVivant:     #Remplissage aléatoire
+                    etatCell[y][x] = True                   #de la map en fonction
+                else:                                       #du pourcentage choisi
+                    etatCell[y][x] = False
+
+    if random == False:
+        mapFichier = contenuFichier.split("\n")     #Tableau de la map
+                                                    #1 ligne sur le txt
+        for x in range(taille):                     #-> 1 case du tableau
+            for y in range(taille):
+
+                if mapFichier[x+1][y] == "0":       #0 sur txt = morte
+                    etatCell[y][x] = False
+
+                elif mapFichier[x+1][y] == "1":     #1 sur txt = vivante
+                    etatCell[y][x] = True
+
 
 
 #Fenetre
@@ -50,52 +92,64 @@ def generation():
 
 def initialisationFenetre():
 
-    print("initialisationFenetre")
 
     for x in range(taille):
         for y in range(taille):
-            if etatCell[x][y] == True:
-                Label(
-                    Fenetre,
+            if etatCell[y][x] == True:          #Si cell vivante
+                Label(Fenetre,                  #case verte (ou couleur choisie)
                     text = "   ",
                     relief = GROOVE,
                     borderwidth = 1,
-                    bg = "chartreuse").grid(
-                        row = y, column = x, sticky = "news")
-            else:
-                Label(
-                    Fenetre,
+                    bg = couleurVivant).grid(
+                    row = y, column = x, sticky = "news")
+
+            elif etatCell[y][x] == False:       #Si cell morte
+                Label(Fenetre,                  #case grise
                     text = "   ",
                     relief = GROOVE,
                     borderwidth = 1,
                     bg = "light grey").grid(
-                        row = y, column = x, sticky = "news")
+                    row = y, column = x, sticky = "news")
+
+            else:
+                print("Erreur Cell ni vivante ni morte")    #Erreur, pas censé arriver
 
 
 def compteur(x, y):
 
-    print("compteur")
-
     cellVivantesAutour = 0
 
-    for xadd in range(-1, 2):
-        for yadd in range(-1, 2):
+    for yadd in range(-1, 2):       #Ligne au dessus, même ligne et ligne en dessous
+        for xadd in range(-1, 2):   #Pareil pour colonnes
 
-            if x + xadd >=  taille and y + yadd >=  taille:
-                if etatCell[x + xadd - taille][y + yadd - taille] == True:
-                    cellVivantesAutour +=  1
+            yfinal = y + yadd
+            xfinal = x + xadd
 
-            elif x + xadd >=  taille:
-                if etatCell[x + xadd - taille][y + yadd] == True:
-                    cellVivantesAutour +=  1
+            if yfinal >= taille and xfinal >= taille:       #Si dépasse de la map x & y, reviens de l'autre côté
+                print(etatCell[yfinal - taille][xfinal - taille])
+                if etatCell[yfinal - taille][xfinal - taille] == True:
+                    cellVivantesAutour += 1
 
-            elif y + yadd >=  taille:
-                if etatCell[x + xadd][y + yadd - taille] == True:
-                    cellVivantesAutour +=  1
+            elif yfinal >= taille:                          #Si dépasse de la map en y, reviens de l'autre côté
+                print(etatCell[yfinal - taille][xfinal])
+                if etatCell[yfinal - taille][xfinal] == True:
+                    cellVivantesAutour += 1
 
-            elif x !=  0 or y !=  0:
-                if etatCell[x + xadd][y + yadd] == True:
-                    cellVivantesAutour +=  1
+            elif xfinal >= taille:                          #Si dépasse de la map en x, reviens de l'autre côté
+                print(etatCell[yfinal][xfinal - taille])
+                if etatCell[yfinal][xfinal - taille] == True:
+                    cellVivantesAutour += 1
+
+            elif yadd != 0 or xadd != 0:                    #On compte les cases autour mais pas la case elle même
+                print(etatCell[yfinal][xfinal])
+                if etatCell[yfinal][xfinal] == True:
+                    cellVivantesAutour += 1
+
+            elif yadd == 0 and xadd == 0:
+                print("case")
+                pass
+
+        print("")
 
     return cellVivantesAutour
 
@@ -103,42 +157,45 @@ def compteur(x, y):
 def etapeSuivante():
     global etatCell, etatCell2
 
-    print("etapeSuivante")
 
     for x in range(taille):
         for y in range(taille):
+
             cellVivantesAutour = compteur(x, y)
 
-            if cellVivantesAutour == 3:
-                etatCell2[x][y] == True
+            print(cellVivantesAutour)
 
-            if cellVivantesAutour == 2 and etatCell[x][y] == True:
-                etatCell2[x][y] == True
+            if cellVivantesAutour == 3:
+                etatCell2[y][x] = True
+
+            if cellVivantesAutour == 2 and etatCell[y][x] == True:
+                etatCell2[y][x] = True
 
             else:
-                etatCell2[x][y] = False
+                etatCell2[y][x] = False
+
+        print("")
 
     etatCell = etatCell2
 
 
-def actualisationFenetre():
 
-    print("actualisationFenetre")
+def actualisationFenetre():
 
     for x in range(taille):
         for y in range(taille):
-            if etatCell[x][y] == True:
-                Fenetre.grid_slaves(
-                    row = y, column = x)[0].configure(bg = "chartreuse")
+            if etatCell[y][x] == True:
+                Fenetre.grid_slaves(row = y, column = x)[0].configure(bg = couleurVivant)
 
             else:
-                Fenetre.grid_slaves(
-                    row = y, column = x)[0].configure(bg = "light grey")
+                Fenetre.grid_slaves(row = y, column = x)[0].configure(bg = "light grey")
+
+
 
 
 def changement():
 
-    print("changement")
+
 
     etapeSuivante()
     actualisationFenetre()
@@ -154,13 +211,11 @@ def bouclePrincipale():
 
     if test == 1:
         changement()
-        print("changement bouclePrincipale")
 
     test = 1
 
-    print("bouclePrincipale 2")
 
-    Fenetre.after(pause * 1000, bouclePrincipale)
+    Fenetre.after(int(pause * 1000), bouclePrincipale)
 
 
 #Préparation
@@ -172,7 +227,7 @@ initialisationFenetre()
 
 bouclePrincipale()
 
-Fenetre.mainloop
+Fenetre.mainloop()
 
 print("fin")
 
